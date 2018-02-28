592e0e4f14ada87f6fb3243cf06149fb8d60be32
takahashi5110 <takahashi@electrodream.jp>
2018-02-28 21:06:35 +0900

    no message

--- index.js
+++ index.js
@@ -20,7 +20,14 @@ var api = new ParseServer({
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   },
-  push: JSON.parse(process.env.PARSE_SERVER_PUSH || "{}")
+  push: {
+    ios: {
+      pfx: 'jp.electrodream.PushSampleApp.p12',
+      passphrase: '', // optional password to your p12/PFX
+      bundleId: 'jp.electrodream.PushSampleApp',
+      production: false
+    }
+  }
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
