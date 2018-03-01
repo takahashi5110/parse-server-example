8462c573bfc9b79bc4c254e7601f9bce568ba6e8
takahashi5110 <takahashi@electrodream.jp>
2018-03-01 15:29:53 +0900

    no message

--- cloud/main.js
+++ cloud/main.js
@@ -2,3 +2,27 @@
 Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 });
+
+Parse.Cloud.define("pushsample", function (request, response) {
+  var query = new Parse.Query(Parse.Installation);
+
+  Parse.Push.send(
+    {
+      where: query,
+      data:
+      {
+        alert: 'Test',
+        badge: 1,
+        sound: 'default'
+      }
+    },
+    {
+      success: function() {
+        console.log('##### PUSH OK');
+      },
+      error: function(error) {
+        console.log('##### PUSH ERROR');
+      }
+    },
+    { useMasterKey: true });
+});
