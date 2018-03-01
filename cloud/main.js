f4047d30fcd8640aabe9d08ac304c43b7fec201d
takahashi5110 <takahashi@electrodream.jp>
2018-03-01 18:19:51 +0900

    no message

--- cloud/main.js
+++ cloud/main.js
@@ -15,28 +15,54 @@ Parse.Cloud.define("push", function (request, response) {
            console.log("user objectId:" + _user.objectId);
 
             var query = new Parse.Query(Parse.Installation);
-            query.equalTo('appName',  'HerokuParseApp');
+            query.equalTo('appName', 'HerokuParseApp');
+            query.limit = 1;
+            query.find({
+              success: function(installations) {
+                  console.log(installations[0]);
+                  response.success("done.");
+              },
+              error: function(error) {
+                response.error("Error: " + error.code + " " + error.message);
+              }
+            });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
             // 	// 送信内容
-            var payload = {
-                alert: "HerokuからPUSH",
-                status: "add"
-            };
-
-            Parse.Push.send({
-                data: payload,
-                where: query
-            }, {
-                useMasterKey: true
-            }).then(
-		        function() {
-		        	console.log('push succeeded.');
-    	            response.success("Push Sent! " + log);
-  	          }, function(error) {
-  	          	console.log('push failed.' + error.message);
-    	        response.error("Error while trying to send push " + error.message);
-      	      }
-	        );
+          //   var payload = {
+          //       alert: "HerokuからPUSH",
+          //       status: "add"
+          //   };
+          //
+          //   Parse.Push.send({
+          //       data: payload,
+          //       where: query
+          //   }, {
+          //       useMasterKey: true
+          //   }).then(
+		      //   function() {
+		      //   	console.log('push succeeded.');
+    	    //         response.success("Push Sent! " + log);
+  	      //     }, function(error) {
+  	      //     	console.log('push failed.' + error.message);
+    	    //     response.error("Error while trying to send push " + error.message);
+      	  //     }
+	        // );
 		},
 		error: function(error) {
 			console.log('push failed2.' + error.message);
